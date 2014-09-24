f70f5287cb44e2d159a43eb8ec85970d622dd193
Marius Kristensen <marius.hauglikristensen@broadnet.no>
2014-09-24 19:04:35 +0200

    After fagdag commit

--- kantinesaldo-app/src/com/kwc/kantinesaldo/SaldoActivity.java
+++ kantinesaldo-app/src/com/kwc/kantinesaldo/SaldoActivity.java
@@ -1,24 +1,14 @@
 package com.kwc.kantinesaldo;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.DecimalFormat;
-import java.util.Date;
 
 
 /**
@@ -29,12 +19,53 @@ import java.util.Date;
 public class SaldoActivity extends Activity {
 
     private static final String TAG = "kantinesaldo";
+    private static final String CARDNUMBER = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final TextView saldoTekst = (TextView) findViewById(R.id.saldotekst);
+        final Button theButton = (Button) findViewById(R.id.theButton);
+        final Button saveButton = (Button) findViewById(R.id.saveButton);
+        final EditText cardNumberText = (EditText) findViewById(R.id.cardNumber);
+
+        SharedPreferences myPref = getSharedPreferences("myPref", MODE_PRIVATE);
+        final SharedPreferences.Editor prefSave = myPref.edit();
+
+        cardNumberText.setText(myPref.getString(CARDNUMBER, ""));
+        Log.d(TAG, "Loaded cardnumber: "+myPref.getString(CARDNUMBER, ""));
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefSave.putString(CARDNUMBER, cardNumberText.getText().toString());
+                prefSave.commit();
+            }
+        });
+
+
+        theButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HttpGetBalance httpGetBalance = new HttpGetBalance(getApplicationContext(), "425079021444", "1444") {
+                    @Override
+                    public void onResult(String balance) {
+                        Log.d(TAG, "Balance is " + balance);
+                        cardNumberText.setClickable(true);
+                        saldoTekst.setText(balance);
+                    }
+                };
+                cardNumberText.setClickable(false);
+                httpGetBalance.execute();
+                Log.d(TAG, "Click!");
+            }
+        });
+        saldoTekst.setText("Hello World!");
+
     }
+
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
